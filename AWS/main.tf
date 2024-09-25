c25799e68c2c99b59647a63e31e2fa596d30e3b5
Bhupender Gaur <150256375+bh-gaur@users.noreply.github.com>
2024-09-25 15:48:05 +0530

    Add files via upload

---
+++ AWS/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  backend "s3" {}
+
+  required_version = "0.14.7"
+
+  required_providers {
+    aws = {
+
+        source = "hashicorp/aws"
+
+        version = "~>3.71"
+    }
+  }
+}
+
+provider "aws" {
+    region = var.region
+  
+}
+
+resource "aws_iam_role" "source-datasync-role" {
+    name = var.name
+    assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Action": "sts:AssumeRole",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "datasync.amazonaws.com"
+      }
+    }]
+  })
+}
---
+++ AWS/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+    description = "Region in which role will created"
+    default = "us-east-1"
+}
+
+variable "name" {
+    description = "role name"
+    default = "" 
+}
