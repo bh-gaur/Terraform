36526eba64df5207320b0cfe0745502b1c6540f9
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2025-02-16 23:34:49 +0530

    update provider.tf

--- AWS/EC2/provider.tf
+++ AWS/EC2/provider.tf
@@ -1,13 +1,13 @@
 # Define aws provider 
 
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "~>5.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       version = "~>5.0"
+#     }
+#   }
+# }
 
 provider "aws" {
   region = "us-east-1"
