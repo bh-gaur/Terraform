e2d5ccaf7642b9668fa4daf46aaca35531020464
Vishnu <vishnu@Vishnus-MacBook-Air.local>
2025-02-21 15:18:46 +0530

    update userdata

--- aws-cloud/ec2/main.tf
+++ aws-cloud/ec2/main.tf
@@ -14,36 +14,40 @@ resource "aws_instance" "my_instance" {
     user_data = <<-EOF
               #!/bin/bash
 
-              Update package list
+              # Update package list first
               apt update
 
-              # Install required dependencies
+              # Install dependencies for adding Nginx repository
               apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 
-              # Add the Nginx signing key
+              # Add Nginx signing key
               curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 
-              # Import the key to verify the Nginx repository
-              #gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+              # Import the key for verification
+              gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
 
-              # Add Nginx repository
+              # Add the Nginx repository
               echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] http://nginx.org/packages/ubuntu $(lsb_release -cs) nginx" | tee /etc/apt/sources.list.d/nginx.list
 
-              # Update package list again after adding the new repo
+              # Pin Nginx packages to the Nginx origin
+              echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" | tee /etc/apt/preferences.d/99nginx
+
+              # Update package list again after adding new repo
               apt update
 
               # Install Nginx
               apt install -y nginx
 
-              # Check nginx version to verify installation
+              # Check nginx version
               nginx -v
 
               # Start and enable nginx
               systemctl start nginx
               systemctl enable nginx
 
-              # Check Nginx service status
+              # Verify nginx service status
               systemctl status nginx
+
 
               EOF
 
